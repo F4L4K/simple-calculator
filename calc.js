f7c02b3c2a6bb86e8d1be8bc4c467afb88a871ad
F4L4K <falak25502@gmail.com>
2025-04-10 00:44:05 +0600

    project added

---
+++ calc.js
@@ -0,0 +1,19 @@
+const display=document.getElementById("display")
+
+function appendNumber(number){
+    display.value+=number
+}
+
+function appendOperator(operator)
+{
+    display.value+=operator
+}
+
+function clearDisplay()
+{
+    display.value=""
+}
+
+function calculate(){
+    display.value = eval(display.value)
+}
